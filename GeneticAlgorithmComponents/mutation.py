3806c7d499b1d687509a8d41dbbb39ef3c2b2197
Maria Batrakova <73667985+whatswrongwithyourmitochondria@users.noreply.github.com>
2024-06-02 23:14:38 +0100

    Update mutation.py

--- GeneticAlgorithmComponents/mutation.py
+++ GeneticAlgorithmComponents/mutation.py
@@ -4,6 +4,15 @@ from Utils.constants import MUTATION_RATE
 
 
 def binary_mutation(schedule):
+    """
+    Performs binary mutation on a schedule by randomly changing the timeslot, room, and professor of each class.
+    
+    Parameters:
+        schedule (Schedule): The schedule to mutate.
+    
+    Returns:
+        Schedule: The mutated schedule.
+    """
     data = schedule.data
     for cls in schedule.get_classes():
         if rnd.random() < MUTATION_RATE:
@@ -13,6 +22,15 @@ def binary_mutation(schedule):
     return schedule
 
 def swap_mutation(schedule):
+    """
+    Performs swap mutation on a schedule by randomly swapping the rooms of two classes.
+    
+    Parameters:
+        schedule (Schedule): The schedule to mutate.
+    
+    Returns:
+        Schedule: The mutated schedule.
+    """
     classes_tmp = schedule.get_classes()
     i = rnd.randrange(len(classes_tmp))
     j = rnd.randrange(len(classes_tmp))
