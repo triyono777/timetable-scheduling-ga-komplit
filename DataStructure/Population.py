0ae4435eea97d45d807a867fdc5a2e001b023502
Maria Batrakova <73667985+whatswrongwithyourmitochondria@users.noreply.github.com>
2024-06-02 23:19:29 +0100

    Update Population.py

--- DataStructure/Population.py
+++ DataStructure/Population.py
@@ -2,6 +2,13 @@ from DataStructure.Schedule import Schedule
 
 
 class Population:
+    """
+    Represents a population of schedules in the scheduling system.
+    
+    Attributes:
+        size (int): The size of the population.
+        schedules (list): The list of schedules in the population.
+    """
     def __init__(self, size, data):
         self.size = size
         self.schedules = [Schedule(data).initialize() for _ in range(size)]
