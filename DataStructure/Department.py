9cf58789793b23a0273d6a4f73c95a249e18fdad
Maria Batrakova <73667985+whatswrongwithyourmitochondria@users.noreply.github.com>
2024-06-02 23:18:55 +0100

    Update Department.py

--- DataStructure/Department.py
+++ DataStructure/Department.py
@@ -1,4 +1,11 @@
 class Department:
+    """
+    Represents a department in the scheduling system.
+    
+    Attributes:
+        name (str): The name of the department.
+        courses (list): The list of courses offered by the department.
+    """
     def __init__(self, name, courses):
         self.name = name
         self.courses = courses
