178e96a17889e1711269636dd1a2f47f05af6b9f
Maria Batrakova <73667985+whatswrongwithyourmitochondria@users.noreply.github.com>
2024-06-02 23:23:38 +0100

    Update Timeslot.py

--- DataStructure/Timeslot.py
+++ DataStructure/Timeslot.py
@@ -1,4 +1,11 @@
 class TimeSlot:
+    """
+    Represents a time slot in the scheduling system.
+    
+    Attributes:
+        id (int): The unique identifier for the time slot.
+        time (str): The specific time of the time slot.
+    """
     def __init__(self, id, time):
         self.id = id
         self.time = time
