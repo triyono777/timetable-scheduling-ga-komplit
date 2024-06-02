0b59a954a3688ce6fa1b1b261c05cdffb2f75fda
Maria Batrakova <73667985+whatswrongwithyourmitochondria@users.noreply.github.com>
2024-06-02 23:18:17 +0100

    Update Course.py

--- DataStructure/Course.py
+++ DataStructure/Course.py
@@ -1,4 +1,15 @@
 class Course:
+    """
+    Represents a course in the scheduling system.
+    
+    Attributes:
+        number (str): The unique number identifier for the course.
+        name (str): The name of the course.
+        professors (list): The list of professors who can teach the course.
+        max_number_of_students (int): The maximum number of students that can enroll in the course.
+        preferred_timeslots (list): The list of preferred timeslots for the course (default is an empty list).
+        preferred_rooms (list): The list of preferred rooms for the course (default is an empty list).
+    """
     def __init__(self, number, name, professors, max_number_of_students, preferred_timeslots=[], preferred_rooms=[]):
         self.number = number
         self.name = name
